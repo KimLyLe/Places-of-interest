f42f92d2c36aca1a1489d0b2614c8e3540151f38
kim-ly <SoSe2018>
2019-09-09 17:40:01 +0200

    Sets Android API to 21

---
+++ app/src/main/java/com/example/places_of_interest/PlacesOfInterestActivity.kt
@@ -0,0 +1,12 @@
+package com.example.places_of_interest
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PlacesOfInterestActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
